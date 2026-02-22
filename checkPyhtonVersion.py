0e1376ef9000b4842782e1ee4d6cfdbd62e9fa8d
shivani <dikondwarshivani22@gmail.com>
2026-02-22 20:05:26 +0530

    files

---
+++ checkPyhtonVersion.py
@@ -0,0 +1,7 @@
+import sys
+import platform
+
+print("--- Python Details ---")
+print(f"Version: {sys.version}")
+print(f"Executable Path: {sys.executable}")
+print(f"Platform: {platform.platform()}")
